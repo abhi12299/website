8379f3e7839761ee1442e685b5c7f07ae11297c7
Abhishek Mehandiratta <abhishek.m@so.city>
2019-09-07 23:43:47 +0530

    split css into chunks based on pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const withCSS = require('@zeit/next-css');
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next-server/constants');
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins([withCSS]);
+module.exports = withPlugins([
+    [withCSS, { shouldMergeChunks: false }, [PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD]]
+]);
