ab08859a613f5580335b775431c7eb26e9d17a94
Abhishek Mehandiratta <abhishek.m@so.city>
2019-09-07 23:55:27 +0530

    tried to disable zoom

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
                     <meta charSet="utf-8" />
                     <meta
                         name="viewport"
-                        content="width=device-width, initial-scale=1"
+                        content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"
                     />
                     {/* <link rel="icon" type="image/x-icon" href="../static/favicon.ico" /> */}
                     <link type='stylesheet' href='../node_modules/font-awesome/css/font-awesome.css'/>
