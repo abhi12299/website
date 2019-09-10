c7bfb5c64fffd8b669b10dd90b81e27c6f2cfba7
Abhishek Mehandiratta <abhishek.m@so.city>
2019-09-11 00:50:42 +0530

    minor fixes

--- components/techStack.js
+++ components/techStack.js
@@ -13,7 +13,7 @@ const TechStack = () => {
                     <div className='d-none d-lg-block d-md-block col-lg-5 col-md-5 text-center my-auto'>
                         <img src='../static/png/tech-stack-left-banner.png' alt='banner-left' className='banner-right-tech-stack' />
                     </div>
-                    <div className='offset-md-1 offset-lg-1 col-lg-6 col-md-6 col-sm-12 tech-stack-content' >
+                    <div className='offset-md-1 offset-lg-1 col-lg-6 col-md-6 tech-stack-content' >
                         <h2 className='tech-stack-title'>Technologies I Work With</h2>
                         <div className='row'>
                             {
