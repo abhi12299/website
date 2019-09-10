bebc46af204f29544893656226a13db715053adb
Abhishek Mehandiratta <abhishek.m@so.city>
2019-09-11 00:50:50 +0530

    added projects section

---
+++ constants/myProjects.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        title: 'GRPC & React Integration',
+        description: 'Google\'s RPC implementation combined with React frontend. Doubles up as an activity tracking server as well, using Kafka and Redis (example for redis in different branch).',
+        links: [
+            {
+                name: 'Front End',
+                url: 'https://github.com/abhi12299/gRPC-Tracking-Client'
+            },
+            {
+                name: 'Server',
+                url: 'https://github.com/abhi12299/gRPC-Tracking-Server'
+            }
+        ]
+    },
+    {
+        title: 'Real Time Chat App',
+        description: 'Built with React and Firebase, this serverless app does what the title says. Besides the basic functionality, I also implemented undoing/editing sent messages by double tapping on them.',
+        links: [
+            {
+                name: 'Check It Out',
+                url: 'https://chatapp-e1da9.firebaseapp.com/'
+            },
+            {
+                name: 'Full Code',
+                url: 'https://github.com/abhi12299/chat-app-final'
+            }
+        ]
+    },
+    {
+        title: 'YouTube To MP3',
+        description: 'One of my initial projects. Does what it says. Built with React, NodeJS, youtube-dl. Deployed on Heroku.',
+        links: [
+            {
+                name: 'Check It Out',
+                url: 'https://ytmp3react.herokuapp.com/'
+            },
+            {
+                name: 'Front End',
+                url: 'https://github.com/abhi12299/Youtube-to-MP3-React'
+            },
+            {
+                name: 'Server',
+                url: 'https://github.com/abhi12299/Youtube-to-MP3-Node'
+            },
+        ]
+    },
+    {
+        title: 'Live Coding App',
+        description: 'One of my initial projects. A serverless app to code collectively across machines. A room can have number of participants.',
+        links: [
+            {
+                name: 'Check It Out',
+                url: 'https://interactive-coding.firebaseapp.com/'
+            },
+            {
+                name: 'Full Code',
+                url: 'https://github.com/abhi12299/interactive-coding-app'
+            }
+        ]
+    }
+];
