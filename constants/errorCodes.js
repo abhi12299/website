033873557fd7672ba8eca3bbc406684cf3d74424
Abhishek Mehandiratta <abhishek.m@so.city>
2019-09-28 20:30:56 +0530

    refactor errorCodes

---
+++ constants/errorCodes.js
@@ -0,0 +1,7 @@
+module.exports = [
+    'NOTADMIN', // middleware handled
+    'SESSIONNOTFOUND',
+    'SESSIONTIMEOUT',
+    'THROTTLE',  // middleware handled
+    'EBADCSRFTOKEN',  // middleware handled
+];
