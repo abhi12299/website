0faf16ece1a59ce4edd9c9b76f2b42daeaa6e364
Abhishek Mehandiratta <abhishek.m@so.city>
2019-10-27 09:55:24 +0530

    added metaKeyword validation

--- utils/serverValidations.js
+++ utils/serverValidations.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-function validatePost({title, headerImageURL, 
+function validatePost({title, headerImageURL, metaKeywords,
     metaDescription, postedDate = Date.now(), body, published = 0}) {
     const postSchema = Joi.object({
         title: Joi.string().required(),
@@ -8,11 +8,13 @@ function validatePost({title, headerImageURL,
         metaDescription: Joi.string().required(),
         postedDate: Joi.number().required(),
         body: Joi.string().required(),
-        published: Joi.number().allow(0, 1).required()
+        published: Joi.number().allow(0, 1).required(),
+        metaKeywords: Joi.array().items(Joi.string()).required()
     });
 
     const { error } = Joi.validate({
-        title, headerImageURL, metaDescription, postedDate, body, published
+        title, headerImageURL, metaDescription, 
+        postedDate, body, published, metaKeywords
     }, postSchema);
     return error;
 }
