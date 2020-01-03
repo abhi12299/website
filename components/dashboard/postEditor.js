aa307fd13dfe696848ced393dbb820589e60ae24
Abhishek <abhi.9811206369@gmail.com>
2020-01-03 23:16:38 +0530

    extract upload btn to separate component

--- components/dashboard/postEditor.js
+++ components/dashboard/postEditor.js
@@ -1,33 +1,21 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Editor } from '@tinymce/tinymce-react';
-import dynamic from 'next/dynamic';
+
+import UploadMediaButton from './uploadMediaButton';
 
 import apiKeys from '../../constants/apiKeys';
 import { validatePostTitle, validatePostBody } from '../../utils/validate';
 import { SETTITLE, SETBODY } from '../../redux/types';
-import baseURL from '../../constants/apiURL';
-import { showToast } from '../../utils/toasts';
-import copyToClipboard from '../../utils/copyToClipboard';
-import checkFileType from '../../utils/checkfileType';
-import uploadMedia from '../../utils/uploadMedia';
-
-const ImageCropper = dynamic(() => import('./imageCropper.js'), { ssr: false });
 
 import '../../css/dashboard/postEditor.css';
 
-// for the file to be uploaded!
-let filename = '';
 function PostEditor(props) {
     const { title, body, postRestored, saving } = props;
-    let [imageBlob, setImageBlob] = useState('');
-    let [isMediaUploading, setIsMediaUploading] = useState(false);
 
     const titleInput = useRef();
     const titleError = useRef();
     const bodyError = useRef();
-    const mediaUploadInput = useRef();
-    const uploadStatusRef = useRef();
 
     useEffect(() => {
         if (postRestored) {
@@ -76,105 +64,6 @@ function PostEditor(props) {
     const handleTitleChange = e => 
         props.dispatch({ type: SETTITLE, payload: e.target.value });
 
-    const handlePostMedia = e => {
-        const { files } = e.target;
-        if (files && files.length > 0) {
-            const file = files[0];
-            const type = checkFileType(file);
-            filename = file.name;
-            switch(type) {
-                case'image-gif':
-                    upload(file, filename);
-                    break;
-                case 'image':
-                    let url;
-                    if (URL) {
-                        url = URL.createObjectURL(file);
-                        setImageBlob(url);
-                    } else if (FileReader) {
-                        reader = new FileReader();
-                        reader.onload = function (e) {
-                            url = reader.result;
-                            setImageBlob(url);
-                        };
-                        reader.readAsDataURL(file);
-                    }
-                    break;
-                case 'video':
-                    const fileSizeInMB = file.size * 1e-6;
-                    if (fileSizeInMB >= 20) {
-                        showToast('File size too large!', 'error');
-                        return;
-                    }
-                    upload(file, filename);
-                    break;
-                default:
-                    showToast('Unknown file type!', 'error');
-            }
-        }
-    };
-
-    const isBlobImage = blob => {
-        const mimeType = blob.type;
-        return /^image\/.*/i.test(mimeType);
-    };
-
-    const closeImageCropper = () => {
-        setImageBlob('');
-    }
-
-    const handleUploadProgress = e => {
-        const percent = (e.loaded / e.total) * 100;
-        const progress = Math.round(percent);
-        uploadStatusRef.current.innerText = `${progress}% done`;
-        setIsMediaUploading(true);
-    };
-
-    const handleUploadComplete = resp => {
-        if (!resp.error) {
-            const { path } = resp;
-            copyToClipboard(baseURL + path);
-            showToast('Upload successful! Link copied!', 'success');
-        } else {
-            showToast('Upload successful! Link could not be copied', 'info');
-        }
-        uploadStatusRef.current.innerText = 'Upload';
-        mediaUploadInput.current.value = '';
-        setIsMediaUploading(false);
-    };
-
-    const handleUploadError = () => {
-        showToast('Upload failed!', 'error');
-        uploadStatusRef.current.innerText = 'Upload';
-        mediaUploadInput.current.value = '';
-        setIsMediaUploading(false);
-    };
-
-    const upload = (blob, filename) => {
-        if (blob) {
-            const blobSizeInMB = blob.size * 1e-6;
-            // imageCropper onComplete calls it directly
-            // hence the check
-            if (isBlobImage(blob) && blobSizeInMB >= 5) {
-                showToast('File size too large!', 'error');
-                mediaUploadInput.current.value = '';
-                return;
-            }
-            uploadMedia(blob, filename, {
-                cbOnProgress: handleUploadProgress,
-                cbOnComplete: handleUploadComplete,
-                cbOnError: handleUploadError
-            });
-        } else {
-            showToast('Something went wrong!', 'error');
-        }
-    }
-
-    const handleImageCropCancel = () => {
-        showToast('Upload cancelled!', 'info');
-        mediaUploadInput.current.value = '';
-    };
-
     return (
         <div className='col-lg-9 col-md-8 col-sm-12 editor-container'>
             <div className='post-title-container'>
@@ -191,27 +80,10 @@ function PostEditor(props) {
                     />
                 </h3>
                 <small ref={titleError} className='errorText'></small>
-                <div className='my-auto post-media-upload-container'>
-                    <label className='post-media-upload-label my-auto'>
-                        <input 
-                            disabled={isMediaUploading}
-                            ref={mediaUploadInput}
-                            type='file' 
-                            className='post-media-upload' 
-                            onChange={handlePostMedia} 
-                            accept='image/*, video/*' 
-                        />
-                        <span ref={uploadStatusRef}>Upload</span>
-                    </label>
+                <div className='my-auto'>
+                    <UploadMediaButton />
                 </div>
             </div>
-            <ImageCropper 
-                show={!!imageBlob} 
-                blob={imageBlob}
-                onClose={closeImageCropper} 
-                onComplete={blob => upload(blob, filename)}
-                onCancel={handleImageCropCancel}
-            />
             <Editor
                 apiKey={apiKeys ? apiKeys.TINY_MCE_API_KEY : ''}
                 init={{
---
+++ components/dashboard/uploadMediaButton.js
@@ -0,0 +1,159 @@
+import React, { useState, useRef, Fragment } from 'react';
+import dynamic from 'next/dynamic';
+
+import PropTypes from 'prop-types';
+
+const ImageCropper = dynamic(() => import('./imageCropper'), { ssr: false });
+
+import checkfileType from '../../utils/checkfileType';
+import { showToast } from '../../utils/toasts';
+import uploadMedia from '../../utils/uploadMedia';
+import copyToClipboard from '../../utils/copyToClipboard';
+import baseURL from '../../constants/apiURL';
+
+import '../../css/dashboard/uploadMediaButton.css';
+
+function noop() {}
+let filename = '';
+
+function UploadMediaButton(props) {
+    const { onUploadComplete=noop } = props;
+
+    let [isMediaUploading, setIsMediaUploading] = useState(false);
+    let [imageBlob, setImageBlob] = useState('');
+
+    const mediaUploadInput = useRef();
+    const uploadStatusRef = useRef();
+
+    const handleMedia = e => {
+        const { files } = e.target;
+        if (files && files.length > 0) {
+            const file = files[0];
+            const type = checkfileType(file);
+            filename = file.name;
+            switch(type) {
+                case'image-gif':
+                    upload(file, filename);
+                    break;
+                case 'image':
+                    let url;
+                    if (URL) {
+                        url = URL.createObjectURL(file);
+                        setImageBlob(url);
+                    } else if (FileReader) {
+                        reader = new FileReader();
+                        reader.onload = function (e) {
+                            url = reader.result;
+                            setImageBlob(url);
+                        };
+                        reader.readAsDataURL(file);
+                    }
+                    break;
+                case 'video':
+                    const fileSizeInMB = file.size * 1e-6;
+                    if (fileSizeInMB >= 20) {
+                        showToast('File size too large!', 'error');
+                        return;
+                    }
+                    upload(file, filename);
+                    break;
+                default:
+                    showToast('Unknown file type!', 'error');
+            }
+        }
+    };
+
+    const handleUploadProgress = e => {
+        const percent = (e.loaded / e.total) * 100;
+        const progress = Math.round(percent);
+        uploadStatusRef.current.innerText = `${progress}% done`;
+        setIsMediaUploading(true);
+    };
+
+    const handleUploadComplete = resp => {
+        if (!resp.error) {
+            const { path } = resp;
+            copyToClipboard(baseURL + path);
+            showToast('Upload successful! Link copied!', 'success');
+        } else {
+            showToast('Upload successful! Link could not be copied', 'info');
+        }
+        onUploadComplete();
+
+        uploadStatusRef.current.innerText = 'Upload';
+        mediaUploadInput.current.value = '';
+        setIsMediaUploading(false);
+    };
+
+    const handleUploadError = () => {
+        showToast('Upload failed!', 'error');
+        uploadStatusRef.current.innerText = 'Upload';
+        mediaUploadInput.current.value = '';
+        setIsMediaUploading(false);
+    };
+
+    const upload = (blob, filename) => {
+        if (blob) {
+            const blobSizeInMB = blob.size * 1e-6;
+            // imageCropper onComplete calls it directly
+            // hence the check
+            if (isBlobImage(blob) && blobSizeInMB >= 5) {
+                showToast('File size too large!', 'error');
+                mediaUploadInput.current.value = '';
+                return;
+            }
+            uploadMedia(blob, filename, {
+                cbOnProgress: handleUploadProgress,
+                cbOnComplete: handleUploadComplete,
+                cbOnError: handleUploadError
+            });
+        } else {
+            showToast('Something went wrong!', 'error');
+        }
+    };
+
+    const isBlobImage = blob => {
+        const mimeType = blob.type;
+        return /^image\/.*/i.test(mimeType);
+    };
+
+    const closeImageCropper = () => {
+        setImageBlob('');
+    };
+
+    const handleImageCropCancel = () => {
+        showToast('Upload cancelled!', 'info');
+        mediaUploadInput.current.value = '';
+    };
+
+    return (
+        <Fragment>
+            <ImageCropper
+                show={!!imageBlob} 
+                blob={imageBlob}
+                onClose={closeImageCropper} 
+                onComplete={blob => upload(blob, filename)}
+                onCancel={handleImageCropCancel}
+            />
+            <div className='media-upload-container'>
+                <label className='media-upload-label my-auto'>
+                    <input 
+                        disabled={isMediaUploading}
+                        ref={mediaUploadInput}
+                        type='file' 
+                        className='media-upload' 
+                        onChange={handleMedia} 
+                        accept='image/*, video/*' 
+                    />
+                    <span ref={uploadStatusRef}>Upload</span>
+                </label>
+            </div>
+        </Fragment>
+    );
+};
+
+UploadMediaButton.propTypes = {
+    onUploadComplete: PropTypes.func
+};
+
+export default UploadMediaButton;
