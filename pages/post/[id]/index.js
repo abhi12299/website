3966cc5a7da42d758273d797ed64f69a145abf9a
Abhishek <abhi.9811206369@gmail.com>
2020-01-16 23:58:39 +0530

    /preview, /post files added

---
+++ pages/post/[id]/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+      <h1>Post: {id}</h1>
+  );
+};
+
+export default Post
