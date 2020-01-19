8d59d701b398383568e45725590ffa94a3551b71
Abhishek <abhi.9811206369@gmail.com>
2020-01-19 11:40:38 +0530

    add basic page transition

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from 'next/app';
 import { Provider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
+import { PageTransition } from 'next-page-transitions'
 
 import { initStore } from '../redux';
 
@@ -17,7 +18,28 @@ export default withRedux(initStore, { debug: false })(
       const { Component, pageProps, store } = this.props;
       return (
         <Provider store={store}>
-          <Component {...pageProps} />
+          <PageTransition 
+            timeout={50} 
+            classNames="page-transition"
+          >
+            <Component {...pageProps} />
+          </PageTransition>
+          <style jsx global>{`
+            .page-transition-enter {
+              opacity: 0;
+            }
+            .page-transition-enter-active {
+              opacity: 1;
+              transition: opacity 150ms;
+            }
+            .page-transition-exit {
+              opacity: 1;
+            }
+            .page-transition-exit-active {
+              opacity: 0;
+              transition: opacity 150ms;
+            }
+        `}</style>
         </Provider>
       );
     }
