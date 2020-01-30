1950707551e4e71694a963d3d4ef813892186bb6
Abhishek <abhi.9811206369@gmail.com>
2020-01-30 11:20:55 +0530

    fix log message on createIndex

--- elasticClient/createIndex.js
+++ elasticClient/createIndex.js
@@ -21,7 +21,7 @@ async function createIndex() {
             index: 'post',
             body: { properties: schema } 
         });
-        console.log('index created successfully!');
+        console.log('index created successfully! Please do a bulkIndex to index all posts again');
     } catch (err) {
         logger.error('Error while creating index!', err);
         return err;
